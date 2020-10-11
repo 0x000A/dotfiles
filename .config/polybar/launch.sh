2c206f6cc1fcceb91dffce3ce4471be9282892ed
Imad Eddine Rezgui <imad.rezgui@etu.sorbonne-universite.fr>
2020-10-11 21:11:35 +0200

    update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,6 +8,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-#polybar main -c ~/.config/polybar/config.ini &
-polybar -r main -c ~/.config/polybar/config2.ini &
+# Launch bar
+polybar -r main -c ~/.config/polybar/config.ini &
